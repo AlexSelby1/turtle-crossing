f0164512e26ac9674a0b305a79556ea248b0a076
Alex Selby <alex.selby@hotmail.com>
2021-10-08 09:19:00 +0100

    add player class with moving function

---
+++ player.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.pu()
+        self.goto(STARTING_POSITION)
+        self.color("black")
+        self.seth(90)
+        
+        
+    def move(self):
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
